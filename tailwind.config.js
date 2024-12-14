df096f7170e049a7cd304990f2a7264d5212b9f6
AQodS <qoddri@gmail.com>
2024-12-14 14:35:33 +0700

    feat: navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        "pink-custom": "#FE9AA0",
+      },
+    },
   },
   plugins: [],
 };
